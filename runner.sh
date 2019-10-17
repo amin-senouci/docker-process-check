59c8573048ae03b3a810fa5a91258f2596d59705
ProtonMail - Amin SENOUCI <senhiam16@gmail.com>
2019-10-17 07:17:26 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -5,11 +5,11 @@
 #################################################################################################################################
 
 ## Can be run with Docker
-#docker build -f Dockerfile -t v/nexthink-check-ws:1.0.0 .
-#docker run -p 8080:8080  -it --rm -v "$PWD":/nexthink-check-ws  -w /nexthink-check-ws  v/nexthink-check-ws:1.0.0  mvn -U spring-boot:run
+docker build -f Dockerfile -t v/nexthink-check-ws:1.0.0 .
+docker run -p 8080:8080  -it --rm -v "$PWD":/nexthink-check-ws  -w /nexthink-check-ws  v/nexthink-check-ws:1.0.0  mvn -U spring-boot:run
 
 ###################
 
 
 ## Or, directly with mvn to track server process performance
- mvn -U spring-boot:run
+# mvn -U spring-boot:run
